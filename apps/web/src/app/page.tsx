f94498680fabd7cc7237056bf5a52f21a7d955d1
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2023-08-14 18:43:55 +0100

    feat(web): Fix duplicate class

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -33,7 +33,7 @@ export const metadata: Metadata = {
 
 export default function Home() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2 py-4">
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <main className="w-auto px-4 pt-16 pb-8 mx-auto sm:pt-24 lg:px-8">
                 <h1 className="mx-auto text-6xl font-extrabold tracking-tight text-center text-white sm:text-7xl lg:text-8xl xl:text-8xl">
                     Web
